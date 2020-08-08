54768666940a319523968f6984c8c44dd4e0a623
kai-ogita <kaiogita@gmail.com>
2020-08-08 10:30:52 +0900

    add setting bar

--- src/index.js
+++ src/index.js
@@ -3,26 +3,7 @@
  */
 require('./index.css').toString();
 
-/**
- * Base Paragraph Block for the Editor.js.
- * Represents simple paragraph
- *
- * @author CodeX (team@codex.so)
- * @copyright CodeX 2018
- * @license The MIT License (MIT)
- */
 
-/**
- * @typedef {object} ParagraphConfig
- * @property {string} placeholder - placeholder for the empty paragraph
- * @property {boolean} preserveBlank - Whether or not to keep blank paragraphs when saving editor data
- */
-
-/**
- * @typedef {Object} ParagraphData
- * @description Tool's input and output data format
- * @property {String} text — Paragraph's content. Can include HTML tags: <a><b><i>
- */
 class Paragraph {
   /**
    * Default placeholder for Paragraph Tool
@@ -35,6 +16,30 @@ class Paragraph {
   }
 
   /**
+   * Allowed quote alignments
+   *
+   * @public
+   * @returns {{left: string, center: string}}
+   */
+  static get ALIGNMENTS() {
+    return {
+      left: 'left',
+      center: 'center',
+      right: 'right',
+    };
+  }
+
+  /**
+   * Default quote alignment
+   *
+   * @public
+   * @returns {string}
+   */
+  static get DEFAULT_ALIGNMENT() {
+    return Paragraph.ALIGNMENTS.left;
+  }
+
+  /**
    * Render plugin`s main Element and fill it with saved data
    *
    * @param {object} params - constructor params
@@ -43,24 +48,53 @@ class Paragraph {
    * @param {object} params.api - editor.js api
    */
   constructor({data, config, api}) {
+    console.log("consttt")
     this.api = api;
 
     this._CSS = {
       block: this.api.styles.block,
-      wrapper: 'ce-paragraph'
-    };
-    this.onKeyUp = this.onKeyUp.bind(this);
+      wrapper: 'ce-paragraph',
+    }
+    this.CSS = {
+      baseClass: this.api.styles.block,
+      loading: this.api.styles.loader,
+      input: this.api.styles.input,
+      settingsButton: this.api.styles.settingsButton,
+      settingsButtonActive: this.api.styles.settingsButtonActive,
+    }
+
+    console.log(this.api.styles)
+
+    this.settings = [
+      {
+        name: 'left',
+        icon: `<svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M15.8 10.592v2.043h2.35v2.138H15.8v2.232h-2.25v-2.232h-2.4v-2.138h2.4v-2.28h2.25v.237h1.15-1.15zM1.9 8.455v-3.42c0-1.154.985-2.09 2.2-2.09h4.2v2.137H4.15v3.373H1.9zm0 2.137h2.25v3.325H8.3v2.138H4.1c-1.215 0-2.2-.936-2.2-2.09v-3.373zm15.05-2.137H14.7V5.082h-4.15V2.945h4.2c1.215 0 2.2.936 2.2 2.09v3.42z"/></svg>`
+      },
+      {
+        name: 'center',
+        icon: `<svg width="17" height="10" viewBox="0 0 17 10" xmlns="http://www.w3.org/2000/svg"><path d="M13.568 5.925H4.056l1.703 1.703a1.125 1.125 0 0 1-1.59 1.591L.962 6.014A1.069 1.069 0 0 1 .588 4.26L4.38.469a1.069 1.069 0 0 1 1.512 1.511L4.084 3.787h9.606l-1.85-1.85a1.069 1.069 0 1 1 1.512-1.51l3.792 3.791a1.069 1.069 0 0 1-.475 1.788L13.514 9.16a1.125 1.125 0 0 1-1.59-1.591l1.644-1.644z"/></svg>`
+      },
+      {
+        name: 'right',
+        icon: `<svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.043 8.265l3.183-3.183h-2.924L4.75 10.636v2.923l4.15-4.15v2.351l-2.158 2.159H8.9v2.137H4.7c-1.215 0-2.2-.936-2.2-2.09v-8.93c0-1.154.985-2.09 2.2-2.09h10.663l.033-.033.034.034c1.178.04 2.12.96 2.12 2.089v3.23H15.3V5.359l-2.906 2.906h-2.35zM7.951 5.082H4.75v3.201l3.201-3.2zm5.099 7.078v3.04h4.15v-3.04h-4.15zm-1.1-2.137h6.35c.635 0 1.15.489 1.15 1.092v5.13c0 .603-.515 1.092-1.15 1.092h-6.35c-.635 0-1.15-.489-1.15-1.092v-5.13c0-.603.515-1.092 1.15-1.092z"/></svg>`
+      }
+    ];
+
+    this.onKeyUp = this.onKeyUp.bind(this)
 
     /**
      * Placeholder for paragraph if it is first Block
      * @type {string}
      */
     this._placeholder = config.placeholder ? config.placeholder : Paragraph.DEFAULT_PLACEHOLDER;
-    this._data = {};
+    this.data = {
+      text: data.text || '',
+      alignment: data.alignment || config.defaultAlignment || Paragraph.DEFAULT_ALIGNMENT
+    };
     this._element = this.drawView();
     this._preserveBlank = config.preserveBlank !== undefined ? config.preserveBlank : false;
 
-    this.data = data;
+    // this.data = data;
   }
 
   /**
@@ -115,7 +149,8 @@ class Paragraph {
    */
   merge(data) {
     let newData = {
-      text : this.data.text + data.text
+      text : this.data.text + data.text,
+      alignment: this.data.alignment,
     };
 
     this.data = newData;
@@ -144,9 +179,10 @@ class Paragraph {
    * @public
    */
   save(toolsContent) {
-    return {
-      text: toolsContent.innerHTML
-    };
+    return Object.assign(this.data, {
+      text: toolsContent.innerHTML,
+      // class: toolsContent.classList[2],
+    });
   }
 
   /**
@@ -179,35 +215,9 @@ class Paragraph {
     return {
       text: {
         br: true,
-      }
+      },
+      alignment: {}
     };
-  }
-
-  /**
-   * Get current Tools`s data
-   * @returns {ParagraphData} Current data
-   * @private
-   */
-  get data() {
-    let text = this._element.innerHTML;
-
-    this._data.text = text;
-
-    return this._data;
-  }
-
-  /**
-   * Store data in plugin:
-   * - at the this._data property
-   * - at the HTML
-   *
-   * @param {ParagraphData} data — data to set
-   * @private
-   */
-  set data(data) {
-    this._data = data || {};
-
-    this._element.innerHTML = this._data.text || '';
   }
 
   /**
@@ -222,6 +232,81 @@ class Paragraph {
     };
   }
 
+  renderSettings(){
+    const wrapper = document.createElement('div');
+    const capitalize = str => str[0].toUpperCase() + str.substr(1);
+
+    /**
+     * .map( tune => {
+      const el = this._make('div', this.CSS.settingsButton, {
+        innerHTML: tune.icon,
+        title: `${capitalize(tune.name)} alignment`
+      });
+
+      el.classList.toggle(this.CSS.settingsButtonActive, tune.name === this.data.alignment);
+
+      wrapper.appendChild(el);
+
+      return el;
+    }).
+     */
+    this.settings.map( tune => {
+      const button = document.createElement('div');
+      button.classList.add('cdx-settings-button');
+      button.innerHTML = tune.icon;
+
+      button.classList.toggle(this.CSS.settingsButtonActive, tune.name === this.data.alignment);
+
+      wrapper.appendChild(button);
+
+      return button;
+    }).forEach( (element, index, elements) => {
+      element.addEventListener('click', () => {
+        this._toggleTune(this.settings[index].name);
+        elements.forEach((el, i) => {
+          const {name} = this.settings[i];
+          console.log(element, name, this.data.alignment)
+          el.classList.toggle(this.CSS.settingsButtonActive, name === this.data.alignment);
+        });
+      });
+    });
+
+    return wrapper;
+  }
+
+
+  /**
+   * @private
+   * Click on the Settings Button
+   * @param {string} tune — tune name from this.settings
+   */
+  _toggleTune(tune) {
+    console.log('Image tune clicked', tune);
+    this.data.alignment = tune;
+    this._acceptTuneView();
+  }
+
+  _acceptTuneView() {
+    // this.settings.forEach( tune => {
+    //   this.wrapper.classList.toggle(tune.name, !!this.data[tune.name]);
+    // });
+  }
+
+  _make(tagName, classNames = null, attributes = {}) {
+    let el = document.createElement(tagName);
+
+    if ( Array.isArray(classNames) ) {
+      el.classList.add(...classNames);
+    } else if( classNames ) {
+      el.classList.add(classNames);
+    }
+
+    for (let attrName in attributes) {
+      el[attrName] = attributes[attrName];
+    }
+
+    return el;
+  }
   /**
    * Icon and title for displaying at the Toolbox
    *
