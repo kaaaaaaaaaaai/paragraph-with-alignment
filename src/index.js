8876de72774774f485294e7b44bdb04dfbf0cf0e
kai-ogita <kaiogita@gmail.com>
2020-08-13 22:47:03 +0900

    fix merge timing

--- src/index.js
+++ src/index.js
@@ -5,293 +5,298 @@ require('./index.css').toString();
 
 
 class Paragraph {
-  /**
-   * Default placeholder for Paragraph Tool
-   *
-   * @return {string}
-   * @constructor
-   */
-  static get DEFAULT_PLACEHOLDER() {
-    return '';
-  }
-
-  /**
-   * Allowed quote alignments
-   *
-   * @public
-   * @returns {{left: string, center: string}}
-   */
-  static get ALIGNMENTS() {
-    return {
-      left:   'left',
-      center: 'center',
-      right:  'right',
-    };
-  }
-
-  /**
-   * Default quote alignment
-   *
-   * @public
-   * @returns {string}
-   */
-  static get DEFAULT_ALIGNMENT() {
-    return Paragraph.ALIGNMENTS.left;
-  }
-
-  /**
-   * Render plugin`s main Element and fill it with saved data
-   *
-   * @param {object} params - constructor params
-   * @param {ParagraphData} params.data - previously saved data
-   * @param {ParagraphConfig} params.config - user config for Tool
-   * @param {object} params.api - editor.js api
-   */
-  constructor({data, config, api}) {
-    console.log("consttt")
-    this.api = api;
-    this.config = config;
-    console.log(data)
-
-    this._CSS = {
-      block: this.api.styles.block,
-      wrapper: 'ce-paragraph',
-      alignment:{
-        left:   'ce-paragraph--left',
-        center: 'ce-paragraph--center',
-        right:  'ce-paragraph--right',
-      }
+    /**
+     * Default placeholder for Paragraph Tool
+     *
+     * @return {string}
+     * @constructor
+     */
+    static get DEFAULT_PLACEHOLDER() {
+        return '';
     }
-    this.CSS = {
-      baseClass: this.api.styles.block,
-      loading: this.api.styles.loader,
-      input: this.api.styles.input,
-      settingsButton: this.api.styles.settingsButton,
-      settingsButtonActive: this.api.styles.settingsButtonActive,
-    }
-
-    console.log(this.api.styles)
-
-    this.settings = [
-      {
-        name: 'left',
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" id="Layer" enable-background="new 0 0 64 64" height="20" viewBox="0 0 64 64" width="20"><path d="m54 8h-44c-1.104 0-2 .896-2 2s.896 2 2 2h44c1.104 0 2-.896 2-2s-.896-2-2-2z"/><path d="m54 52h-44c-1.104 0-2 .896-2 2s.896 2 2 2h44c1.104 0 2-.896 2-2s-.896-2-2-2z"/><path d="m10 23h28c1.104 0 2-.896 2-2s-.896-2-2-2h-28c-1.104 0-2 .896-2 2s.896 2 2 2z"/><path d="m54 30h-44c-1.104 0-2 .896-2 2s.896 2 2 2h44c1.104 0 2-.896 2-2s-.896-2-2-2z"/><path d="m10 45h28c1.104 0 2-.896 2-2s-.896-2-2-2h-28c-1.104 0-2 .896-2 2s.896 2 2 2z"/></svg>`
-      },
-      {
-        name: 'center',
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" id="Layer" enable-background="new 0 0 64 64" height="20" viewBox="0 0 64 64" width="20"><path d="m54 8h-44c-1.104 0-2 .896-2 2s.896 2 2 2h44c1.104 0 2-.896 2-2s-.896-2-2-2z"/><path d="m54 52h-44c-1.104 0-2 .896-2 2s.896 2 2 2h44c1.104 0 2-.896 2-2s-.896-2-2-2z"/><path d="m46 23c1.104 0 2-.896 2-2s-.896-2-2-2h-28c-1.104 0-2 .896-2 2s.896 2 2 2z"/><path d="m54 30h-44c-1.104 0-2 .896-2 2s.896 2 2 2h44c1.104 0 2-.896 2-2s-.896-2-2-2z"/><path d="m46 45c1.104 0 2-.896 2-2s-.896-2-2-2h-28c-1.104 0-2 .896-2 2s.896 2 2 2z"/></svg>`
-      },
-      {
-        name: 'right',
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" id="Layer" enable-background="new 0 0 64 64" height="20" viewBox="0 0 64 64" width="20"><path d="m54 8h-44c-1.104 0-2 .896-2 2s.896 2 2 2h44c1.104 0 2-.896 2-2s-.896-2-2-2z"/><path d="m54 52h-44c-1.104 0-2 .896-2 2s.896 2 2 2h44c1.104 0 2-.896 2-2s-.896-2-2-2z"/><path d="m54 19h-28c-1.104 0-2 .896-2 2s.896 2 2 2h28c1.104 0 2-.896 2-2s-.896-2-2-2z"/><path d="m54 30h-44c-1.104 0-2 .896-2 2s.896 2 2 2h44c1.104 0 2-.896 2-2s-.896-2-2-2z"/><path d="m54 41h-28c-1.104 0-2 .896-2 2s.896 2 2 2h28c1.104 0 2-.896 2-2s-.896-2-2-2z"/></svg>`
-      }
-    ];
-
-    this.onKeyUp = this.onKeyUp.bind(this)
 
     /**
-     * Placeholder for paragraph if it is first Block
-     * @type {string}
+     * Allowed paragraph alignments
+     *
+     * @public
+     * @returns {{left: string, center: string}}
      */
-    this._placeholder = config.placeholder ? config.placeholder : Paragraph.DEFAULT_PLACEHOLDER;
-    this.data = {
-      text: data.text || '',
-      alignment: data.alignment || config.defaultAlignment || Paragraph.DEFAULT_ALIGNMENT
-    };
-    this._element = this.drawView();
-    this._preserveBlank = config.preserveBlank !== undefined ? config.preserveBlank : false;
-
-    // this.data = data;
-  }
-
-  /**
-   * Check if text content is empty and set empty string to inner html.
-   * We need this because some browsers (e.g. Safari) insert <br> into empty contenteditanle elements
-   *
-   * @param {KeyboardEvent} e - key up event
-   */
-  onKeyUp(e) {
-    if (e.code !== 'Backspace' && e.code !== 'Delete') {
-      return;
+    static get ALIGNMENTS() {
+        return {
+            left: 'left',
+            center: 'center',
+            right: 'right',
+        };
     }
 
-    const {textContent} = this._element;
-
-    if (textContent === '') {
-      this._element.innerHTML = '';
-    }
-  }
-
-  /**
-   * Create Tool's view
-   * @return {HTMLElement}
-   * @private
-   */
-  drawView() {
-    let div = document.createElement('DIV');
-
-    div.classList.add(this._CSS.wrapper, this._CSS.block, this._CSS.alignment[this.data.alignment]);
-    div.contentEditable = true;
-    div.dataset.placeholder = this.api.i18n.t(this._placeholder);
-    div.innerHTML = this.data.text;
-
-    div.addEventListener('keyup', this.onKeyUp);
-
-    return div;
-  }
-
-  /**
-   * Return Tool's view
-   * @returns {HTMLDivElement}
-   * @public
-   */
-  render() {
-    return this._element;
-  }
-
-  /**
-   * Method that specified how to merge two Text blocks.
-   * Called by Editor.js by backspace at the beginning of the Block
-   * @param {ParagraphData} data
-   * @public
-   */
-  merge(data) {
-    let newData = {
-      text : this.data.text + data.text,
-      alignment: this.data.alignment,
-    };
-
-    this.data = newData;
-  }
-
-  /**
-   * Validate Paragraph block data:
-   * - check for emptiness
-   *
-   * @param {ParagraphData} savedData — data received after saving
-   * @returns {boolean} false if saved data is not correct, otherwise true
-   * @public
-   */
-  validate(savedData) {
-    if (savedData.text.trim() === '' && !this._preserveBlank) {
-      return false;
+    /**
+     * Default paragraph alignment
+     *
+     * @public
+     * @returns {string}
+     */
+    static get DEFAULT_ALIGNMENT() {
+        return Paragraph.ALIGNMENTS.left;
     }
 
-    return true;
-  }
+    /**
+     * Render plugin`s main Element and fill it with saved data
+     *
+     * @param {object} params - constructor params
+     * @param {ParagraphData} params.data - previously saved data
+     * @param {ParagraphConfig} params.config - user config for Tool
+     * @param {object} params.api - editor.js api
+     */
+    constructor({data, config, api}) {
+        this.api = api;
+        this.config = config;
 
-  /**
-   * Extract Tool's data from the view
-   * @param {HTMLDivElement} toolsContent - Paragraph tools rendered view
-   * @returns {ParagraphData} - saved data
-   * @public
-   */
-  save(toolsContent) {
-    return Object.assign(this.data, {
-      text: toolsContent.innerHTML,
-    });
-  }
+        this._CSS = {
+            block: this.api.styles.block,
+            wrapper: 'ce-paragraph',
+            alignment: {
+                left: 'ce-paragraph--left',
+                center: 'ce-paragraph--center',
+                right: 'ce-paragraph--right',
+            }
+        }
+        this.CSS = {
+            baseClass: this.api.styles.block,
+            loading: this.api.styles.loader,
+            input: this.api.styles.input,
+            settingsButton: this.api.styles.settingsButton,
+            settingsButtonActive: this.api.styles.settingsButtonActive,
+        }
 
-  /**
-   * On paste callback fired from Editor.
-   *
-   * @param {PasteEvent} event - event with pasted data
-   */
-  onPaste(event) {
-    const data = {
-      text: event.detail.data.innerHTML,
-      alignment: this.config.defaultAlignment || Paragraph.DEFAULT_ALIGNMENT
-    };
+        this.settings = [
+            {
+                name: 'left',
+                icon: `<svg xmlns="http://www.w3.org/2000/svg" id="Layer" enable-background="new 0 0 64 64" height="20" viewBox="0 0 64 64" width="20"><path d="m54 8h-44c-1.104 0-2 .896-2 2s.896 2 2 2h44c1.104 0 2-.896 2-2s-.896-2-2-2z"/><path d="m54 52h-44c-1.104 0-2 .896-2 2s.896 2 2 2h44c1.104 0 2-.896 2-2s-.896-2-2-2z"/><path d="m10 23h28c1.104 0 2-.896 2-2s-.896-2-2-2h-28c-1.104 0-2 .896-2 2s.896 2 2 2z"/><path d="m54 30h-44c-1.104 0-2 .896-2 2s.896 2 2 2h44c1.104 0 2-.896 2-2s-.896-2-2-2z"/><path d="m10 45h28c1.104 0 2-.896 2-2s-.896-2-2-2h-28c-1.104 0-2 .896-2 2s.896 2 2 2z"/></svg>`
+            },
+            {
+                name: 'center',
+                icon: `<svg xmlns="http://www.w3.org/2000/svg" id="Layer" enable-background="new 0 0 64 64" height="20" viewBox="0 0 64 64" width="20"><path d="m54 8h-44c-1.104 0-2 .896-2 2s.896 2 2 2h44c1.104 0 2-.896 2-2s-.896-2-2-2z"/><path d="m54 52h-44c-1.104 0-2 .896-2 2s.896 2 2 2h44c1.104 0 2-.896 2-2s-.896-2-2-2z"/><path d="m46 23c1.104 0 2-.896 2-2s-.896-2-2-2h-28c-1.104 0-2 .896-2 2s.896 2 2 2z"/><path d="m54 30h-44c-1.104 0-2 .896-2 2s.896 2 2 2h44c1.104 0 2-.896 2-2s-.896-2-2-2z"/><path d="m46 45c1.104 0 2-.896 2-2s-.896-2-2-2h-28c-1.104 0-2 .896-2 2s.896 2 2 2z"/></svg>`
+            },
+            {
+                name: 'right',
+                icon: `<svg xmlns="http://www.w3.org/2000/svg" id="Layer" enable-background="new 0 0 64 64" height="20" viewBox="0 0 64 64" width="20"><path d="m54 8h-44c-1.104 0-2 .896-2 2s.896 2 2 2h44c1.104 0 2-.896 2-2s-.896-2-2-2z"/><path d="m54 52h-44c-1.104 0-2 .896-2 2s.896 2 2 2h44c1.104 0 2-.896 2-2s-.896-2-2-2z"/><path d="m54 19h-28c-1.104 0-2 .896-2 2s.896 2 2 2h28c1.104 0 2-.896 2-2s-.896-2-2-2z"/><path d="m54 30h-44c-1.104 0-2 .896-2 2s.896 2 2 2h44c1.104 0 2-.896 2-2s-.896-2-2-2z"/><path d="m54 41h-28c-1.104 0-2 .896-2 2s.896 2 2 2h28c1.104 0 2-.896 2-2s-.896-2-2-2z"/></svg>`
+            }
+        ];
 
-    this.data = data;
-  }
+        this.onKeyUp = this.onKeyUp.bind(this)
 
-  /**
-   * Enable Conversion Toolbar. Paragraph can be converted to/from other tools
-   */
-  static get conversionConfig() {
-    return {
-      export: 'text', // to convert Paragraph to other block, use 'text' property of saved data
-      import: 'text' // to covert other block's exported string to Paragraph, fill 'text' property of tool data
-    };
-  }
+        /**
+         * Placeholder for paragraph if it is first Block
+         * @type {string}
+         */
+        this._placeholder = config.placeholder ? config.placeholder : Paragraph.DEFAULT_PLACEHOLDER;
+        this.data = {
+            text: data.text || '',
+            alignment: data.alignment || config.defaultAlignment || Paragraph.DEFAULT_ALIGNMENT
+        };
+        this._element = this.drawView();
+        this._preserveBlank = config.preserveBlank !== undefined ? config.preserveBlank : false;
+    }
 
-  /**
-   * Sanitizer rules
-   */
-  static get sanitize() {
-    return {
-      text: {
-        br: true,
-      },
-      alignment: {}
-    };
-  }
+    /**
+     * Check if text content is empty and set empty string to inner html.
+     * We need this because some browsers (e.g. Safari) insert <br> into empty contenteditanle elements
+     *
+     * @param {KeyboardEvent} e - key up event
+     */
+    onKeyUp(e) {
+        if (e.code !== 'Backspace' && e.code !== 'Delete') {
+            return;
+        }
 
-  /**
-   * Used by Editor paste handling API.
-   * Provides configuration to handle P tags.
-   *
-   * @returns {{tags: string[]}}
-   */
-  static get pasteConfig() {
-    return {
-      tags: [ 'P' ]
-    };
-  }
+        const {textContent} = this._element;
 
-  renderSettings(){
-    const wrapper = document.createElement('div');
+        if (textContent === '') {
+            this._element.innerHTML = '';
+        }
+    }
 
-    this.settings.map( tune => {
-      const button = document.createElement('div');
-      button.classList.add('cdx-settings-button');
-      button.innerHTML = tune.icon;
+    /**
+     * Create Tool's view
+     * @return {HTMLElement}
+     * @private
+     */
+    drawView() {
+        let div = document.createElement('DIV');
 
-      button.classList.toggle(this.CSS.settingsButtonActive, tune.name === this.data.alignment);
+        div.classList.add(this._CSS.wrapper, this._CSS.block, this._CSS.alignment[this.data.alignment]);
+        div.contentEditable = true;
+        div.dataset.placeholder = this.api.i18n.t(this._placeholder);
+        div.innerHTML = this.data.text;
 
-      wrapper.appendChild(button);
+        div.addEventListener('keyup', this.onKeyUp);
 
-      return button;
-    }).forEach( (element, index, elements) => {
+        return div;
+    }
 
-      element.addEventListener('click', () => {
-        console.log(this._element)
-        this._toggleTune(this.settings[index].name);
-        elements.forEach((el, i) => {
-          const {name} = this.settings[i];
-          console.log(element, name, this.data.alignment)
-          el.classList.toggle(this.CSS.settingsButtonActive, name === this.data.alignment);
-          //paragraphのdivにalignmentのclassをつける。
-          this._element.classList.toggle(this._CSS.alignment[name], name === this.data.alignment)
+    /**
+     * Return Tool's view
+     * @returns {HTMLDivElement}
+     * @public
+     */
+    render() {
+        return this._element;
+    }
+
+    /**
+     * Method that specified how to merge two Text blocks.
+     * Called by Editor.js by backspace at the beginning of the Block
+     * @param {ParagraphData} data
+     * @public
+     */
+    merge(data) {
+
+        let newData = {
+            text: this.data.text += data.text,
+            alignment: this.data.alignment,
+        };
+
+        this._element.innerHTML = this.data.text;
+
+        this.data = newData;
+    }
+
+    /**
+     * Validate Paragraph block data:
+     * - check for emptiness
+     *
+     * @param {ParagraphData} savedData — data received after saving
+     * @returns {boolean} false if saved data is not correct, otherwise true
+     * @public
+     */
+    validate(savedData) {
+        if (savedData.text.trim() === '' && !this._preserveBlank) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Extract Tool's data from the view
+     * @param {HTMLDivElement} toolsContent - Paragraph tools rendered view
+     * @returns {ParagraphData} - saved data
+     * @public
+     */
+    save(toolsContent) {
+        return Object.assign(this.data, {
+            text: toolsContent.innerHTML,
         });
-      });
-    });
+    }
 
-    return wrapper;
-  }
+    /**
+     * On paste callback fired from Editor.
+     *
+     * @param {PasteEvent} event - event with pasted data
+     */
+    onPaste(event) {
+        const data = {
+            text: event.detail.data.innerHTML,
+            alignment: this.config.defaultAlignment || Paragraph.DEFAULT_ALIGNMENT
+        };
+
+        this.data = data;
+    }
+
+    /**
+     * Enable Conversion Toolbar. Paragraph can be converted to/from other tools
+     */
+    static get conversionConfig() {
+        return {
+            export: 'text', // to convert Paragraph to other block, use 'text' property of saved data
+            import: 'text' // to covert other block's exported string to Paragraph, fill 'text' property of tool data
+        };
+    }
+
+    /**
+     * Sanitizer rules
+     */
+    static get sanitize() {
+        return {
+            text: {
+                br: true,
+            },
+            alignment: {}
+        };
+    }
+
+    /**
+     * Used by Editor paste handling API.
+     * Provides configuration to handle P tags.
+     *
+     * @returns {{tags: string[]}}
+     */
+    static get pasteConfig() {
+        return {
+            tags: ['P']
+        };
+    }
+
+    /**
+     *
+     * @returns {HTMLDivElement}
+     */
+    renderSettings() {
+        const wrapper = document.createElement('div');
+
+        this.settings.map(tune => {
+            /**
+             * buttonのdomを作成して、alignのtoggleをactiveに設定する
+             * @type {HTMLDivElement}
+             */
+            const button = document.createElement('div');
+            button.classList.add('cdx-settings-button');
+            button.innerHTML = tune.icon;
+
+            button.classList.toggle(this.CSS.settingsButtonActive, tune.name === this.data.alignment);
+
+            wrapper.appendChild(button);
+
+            return button;
+        }).forEach((element, index, elements) => {
+
+            element.addEventListener('click', () => {
+
+                this._toggleTune(this.settings[index].name);
+
+                elements.forEach((el, i) => {
+                    const {name} = this.settings[i];
+                    el.classList.toggle(this.CSS.settingsButtonActive, name === this.data.alignment);
+                    //paragraphのdivにalignmentのclassをつける。
+                    this._element.classList.toggle(this._CSS.alignment[name], name === this.data.alignment)
+                });
+            });
+        });
+
+        return wrapper;
+    }
 
 
-  /**
-   * @private
-   * Click on the Settings Button
-   * @param {string} tune — tune name from this.settings
-   */
-  _toggleTune(tune) {
-    this.data.alignment = tune;
-  }
+    /**
+     * @private
+     * Click on the Settings Button
+     * @param {string} tune — tune name from this.settings
+     */
+    _toggleTune(tune) {
+        this.data.alignment = tune;
+    }
 
-  /**
-   * Icon and title for displaying at the Toolbox
-   *
-   * @return {{icon: string, title: string}}
-   */
-  static get toolbox() {
-    return {
-      icon: require('./toolbox-icon.svg').default,
-      title: 'Text'
-    };
-  }
+    /**
+     * Icon and title for displaying at the Toolbox
+     *
+     * @return {{icon: string, title: string}}
+     */
+    static get toolbox() {
+        return {
+            icon: require('./toolbox-icon.svg').default,
+            title: 'Text'
+        };
+    }
 }
 
 module.exports = Paragraph;
